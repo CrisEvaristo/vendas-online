1aafcdf0a0270330dde4272863b3756a4b31156b
Cristóvão Evaristo <cevaristo@quality.co.ao>
2025-01-02 17:55:29 +0100

    adicionando eslint no projecto

---
+++ .Eslintrc.js
@@ -0,0 +1,45 @@
+const plugin = require("eslint-plugin-react");
+const { jsx } = require("react/jsx-runtime");
+
+module.exports={
+    root:true;
+    parser:'@typescript-eslint/parser',
+    parserOptions:{
+        ecmaVersion:2020,
+        sourceType:'module',
+        ecmaFeature:{
+            jsx:true,
+        },
+    },
+    setting:{
+        react:{
+            version:'detect',
+        },
+        'import/resolver':{
+            node:{
+                paths:['src'],
+                extentions:['.js','.jsx','.ts','.tsx'],
+            },
+        },
+    },
+    env:{
+        browser:true,
+        amd:true,
+        node:true,
+    },
+    extends:[
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins:['simple-import-sort','prettier'],
+    rules:{
+        'prettier/prettier':['error',{},{usePrettierrc:true}],
+        'react/react-in-jsx-scope':'off',
+        'react/prop-types':'off',
+        '@typescript-eslint/explicit-function-return-type':'off',
+        'simple-import-sort/imports':'error',
+        'simple-import-sort/exports':'error',
+    },
+};
